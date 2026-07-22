f6e968b43d7cfdfef26e375f8cb6fbc022f43fe1
Ashley L <lawrence.ashley@proton.me>
2026-07-22 14:09:33 -0400

    Added some of openCollection function in script file... Not done yet

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+$("#type-button").addEventListener("click", displayCollection(event, collectiontype))
+
+function openCollection(evt, collectiontype) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tab-links");
+}
