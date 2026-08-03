71cc0dd505bec5451c24407b784c2969e194c990
Ashley L <lawrence.ashley@proton.me>
2026-08-03 12:29:46 -0400

    Made item-builder.py file and starting code

---
+++ data-builder/item-builder.py
@@ -0,0 +1,19 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+from pathlib import Path
+
+url = "https://webkinzguide.com/wiki/"
+headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) "
+        "Gecko/20100101 Firefox/140.0"
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+}
+
+page = requests.get(url, headers=headers)
+output = Path("../data/collections")
+output.mkdir(parents=True, exist_ok=True)
+soup = BeautifulSoup(page.text, "html.parser")
