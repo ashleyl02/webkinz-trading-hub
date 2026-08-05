9b5da996bbcfcd5045d2729ab6573b50e8beaf4c
Ashley L <lawrence.ashley@proton.me>
2026-08-05 11:02:20 -0400

    item-builder.py now successfully fetches the data, but does not successfully store it in json files yet.
    json files are created, but data is not being properly appended.

--- data-builder/item-builder.py
+++ data-builder/item-builder.py
@@ -3,7 +3,6 @@ import json
 from bs4 import BeautifulSoup
 from pathlib import Path
 
-url = "https://webkinzguide.com/wiki/"
 headers = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) "
@@ -13,7 +12,48 @@ headers = {
     "Accept-Language": "en-US,en;q=0.5",
 }
 
-page = requests.get(url, headers=headers)
-output = Path("../data/collections")
+output = Path("../data/items")
 output.mkdir(parents=True, exist_ok=True)
-soup = BeautifulSoup(page.text, "html.parser")
+
+items = []
+
+with open("../data/collections/type-collections.json", "r") as file: #starting with just one collection, will cover all four later
+    collections = json.load(file)
+
+for collection in collections:
+    url = collection["page_url"]
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.text, "html.parser")
+    table = soup.find("table")
+
+    if table is None:
+        print(f"Skipping {url}")
+        continue
+
+    for row in table.find_all("tr"):
+        cells = row.find_all("td")
+
+        if len(cells) != 3:
+            continue
+
+        img = cells[0].find("img")
+
+        if not img:
+            continue
+
+        image_url = "https:" + img["src"]
+
+        title = cells[0].get_text(strip=True)
+
+        is_tradeable = cells[2].get_text(strip=True) == "Yes"
+
+        items.append({
+            "title": title,
+            "image_url": image_url,
+            "is_tradeable": is_tradeable
+        })
+
+    output_file = output / f"{collection['id']}-items.json"
+    with open(output_file, "w") as final:
+        json.dump(items, final, indent=4)
+    items = []
