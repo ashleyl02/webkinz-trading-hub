68baa5af9eadf7acadc8e115a82f165ce3f08221
Ashley L <lawrence.ashley@proton.me>
2026-08-05 11:30:17 -0400

    item-builder.py now successfully retrieves data for every item in webkinz.

--- data-builder/item-builder.py
+++ data-builder/item-builder.py
@@ -12,49 +12,51 @@ headers = {
     "Accept-Language": "en-US,en;q=0.5",
 }
 
-output = Path("../data/items")
-output.mkdir(parents=True, exist_ok=True)
+collection_groups = ["color", "misc", "theme", "type"]
 
-items = []
-
-with open("../data/collections/type-collections.json", "r") as file: #starting with just one collection, will cover all four later
-    collections = json.load(file)
-
-for collection in collections:
-    url = collection["page_url"]
-    page = requests.get(url, headers=headers)
-    soup = BeautifulSoup(page.text, "html.parser")
-    tables = soup.find_all("table")
-    table = tables[1]
-
-    if table is None:
-        print(f"Skipping {url}")
-        continue
-
-    for row in table.find_all("tr"):
-        cells = row.find_all("td")
-
-        if len(cells) != 3:
-            continue
-
-        img = cells[0].find("img")
-
-        if not img:
-            continue
-
-        image_url = "https:" + img["src"]
-
-        title = cells[0].get_text(strip=True)
-
-        is_tradeable = cells[2].get_text(strip=True) == "Yes"
-
-        items.append({
-            "title": title,
-            "image_url": image_url,
-            "is_tradeable": is_tradeable
-        })
-
-    output_file = output / f"{collection['id']}-items.json"
-    with open(output_file, "w") as final:
-        json.dump(items, final, indent=4)
+for group in collection_groups:
+    output = Path(f"../data/items/{group}-items")
+    output.mkdir(parents=True, exist_ok=True)
     items = []
+
+    with open(f"../data/collections/{group}-collections.json", "r") as file:
+        collections = json.load(file)
+
+    for collection in collections:
+        url = collection["page_url"]
+        page = requests.get(url, headers=headers)
+        soup = BeautifulSoup(page.text, "html.parser")
+        tables = soup.find_all("table")
+        table = tables[1]
+
+        if table is None:
+            print(f"Skipping {url}")
+            continue
+
+        for row in table.find_all("tr"):
+            cells = row.find_all("td")
+
+            if len(cells) != 3:
+                continue
+
+            img = cells[0].find("img")
+
+            if not img:
+                continue
+
+            image_url = "https:" + img["src"]
+
+            title = cells[0].get_text(strip=True)
+
+            is_tradeable = cells[2].get_text(strip=True) == "Yes"
+
+            items.append({
+                "title": title,
+                "image_url": image_url,
+                "is_tradeable": is_tradeable
+            })
+
+        output_file = output / f"{collection['id']}-items.json"
+        with open(output_file, "w") as final:
+            json.dump(items, final, indent=4)
+        items = []
